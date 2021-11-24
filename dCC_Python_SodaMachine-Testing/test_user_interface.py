69cbf5edcc55b1c4f69628f07e0085ccdd2f96f7
CodyLuckmann <cluckmann@live.com>
2021-11-24 10:01:59 -0600

    Added DocStrings for better description of each test

--- dCC_Python_SodaMachine-Testing/test_user_interface.py
+++ dCC_Python_SodaMachine-Testing/test_user_interface.py
@@ -6,26 +6,31 @@ import coins
 class TestValidateMainMenu(unittest.TestCase):
     
     def test_validate_main_menu_1(self):
+        """ Runs test to check when user input is 1 the correct tuple is returned"""
         self.user_interface = user_interface
         person = self.user_interface.validate_main_menu(1)
         self.assertEqual(person, (True, 1))
     
     def test_validate_main_menu_2(self):
+        """ Runs test to check when user input is 2 the correct tuple is returned"""
         self.user_interface = user_interface
         person = self.user_interface.validate_main_menu(2)
         self.assertEqual(person, (True, 2))
     
     def test_validate_main_menu_3(self):
+        """ Runs test to check when user input is 3 the correct tuple is returned"""
         self.user_interface = user_interface
         person = self.user_interface.validate_main_menu(3)
         self.assertEqual(person, (True,  3))
     
     def test_validate_main_menu_4(self):
+        """ Runs test to check when user input is 4 the correct tuple is returned"""
         self.user_interface = user_interface
         person = self.user_interface.validate_main_menu(4)
         self.assertEqual(person, (True, 4))    
     
     def test_not_valid_option(self):
+        """ Runs test to check when user input is not 1-4 returns False, None"""
         self.user_interface = user_interface
         person = self.user_interface.validate_main_menu(5)
         self.assertEqual(person, (False, None))
@@ -33,11 +38,13 @@ class TestValidateMainMenu(unittest.TestCase):
 class TestTryParseInt(unittest.TestCase):
     
     def test_try_parse_int(self):
+        """Runs test to check if string can be parsed into an integer"""
         self.user_interface = user_interface
         number = self.user_interface.try_parse_int(10)
         self.assertEqual(number, 10)
 
     def test_try_parse_int_1(self):
+        """Runs test when string can't be parsed to integer, returns 0"""
         self.user_interface = user_interface
         string = self.user_interface.try_parse_int('hello')
         self.assertEqual(string, 0)
@@ -45,6 +52,7 @@ class TestTryParseInt(unittest.TestCase):
 class TestGetUniqueCanNames(unittest.TestCase):
     
     def test_get_unique_names(self):
+        """Runs test to check when list of cans is passed in of multiple same can names, removes duplicates"""
         self.user_interface = user_interface
         can_list = []
         can_list.append(cans.Cola())
@@ -57,6 +65,7 @@ class TestGetUniqueCanNames(unittest.TestCase):
         self.assertEqual(len(can), 3)
     
     def test_get_unique_names_1(self):
+        """Runs test when empty list is passed in length of list remains at 0"""
         self.user_interface = user_interface
         can_list = []
         can = self.user_interface.get_unique_can_names(can_list)
@@ -65,6 +74,7 @@ class TestGetUniqueCanNames(unittest.TestCase):
 class TestDisplayPaymentValue(unittest.TestCase):
     
     def test_display_payment_value(self):
+        """Runs test to determine when list of coins is added, returns total payment"""
         self.user_interface = user_interface
         coins_list = []
         coins_list.append(coins.Quarter())
@@ -75,6 +85,7 @@ class TestDisplayPaymentValue(unittest.TestCase):
         self.assertEqual(money, .41)
 
     def test_display_payment_value_2(self):
+        """Run test when empty list of coins added, total value equals 0"""
         self.user_interface = user_interface
         coins_list = []
         money = self.user_interface.display_payment_value(coins_list)
@@ -86,31 +97,37 @@ class TestValidateCoinSelection(unittest.TestCase):
         self.user_interface = user_interface
     
     def test_validate_coin_1(self):
+        """Runs test to check when user input is 1, returns correct tuple"""
         self.user_interface = user_interface
         coin = self.user_interface.validate_coin_selection(1)
         self.assertEqual(coin, (True, 'Quarter'))
     
     def test_validate_coin_2(self):
+        """Runs test to check when user input is 2, returns correct tuple"""
         self.user_interface = user_interface
         coin = self.user_interface.validate_coin_selection(2)
         self.assertEqual(coin, (True, 'Dime'))
     
     def test_validate_coin_3(self):
+        """Runs test to check when user input is 3, returns correct tuple"""
         self.user_interface = user_interface
         coin = self.user_interface.validate_coin_selection(3)
         self.assertEqual(coin, (True, 'Nickel'))
     
     def test_validate_coin_4(self):
+        """Runs test to check when user input is 4, returns correct tuple"""
         self.user_interface = user_interface
         coin = self.user_interface.validate_coin_selection(4)
         self.assertEqual(coin, (True, 'Penny'))
     
     def test_validate_coin_5(self):
+        """Runs test to check when user input is 5, returns correct tuple"""
         self.user_interface = user_interface
         coin = self.user_interface.validate_coin_selection(5)
         self.assertEqual(coin, (True, 'Done'))
     
     def test_not_valid_coin(self):
+        """Runs test to check when user input is not betwenn 1-5, returns False, None"""
         self.user_interface = user_interface
         coin = self.user_interface.validate_coin_selection(6)
         self.assertEqual(coin, (False, None))
