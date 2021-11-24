4f2fb63fbc4e5114966f8605644b1238614a72cd
CodyLuckmann <cluckmann@live.com>
2021-11-24 09:44:49 -0600

    Changed AssertTrue to AssertEqual and changed parameters to inclued is equal to the tuple

--- dCC_Python_SodaMachine-Testing/test_user_interface.py
+++ dCC_Python_SodaMachine-Testing/test_user_interface.py
@@ -8,27 +8,27 @@ class TestValidateMainMenu(unittest.TestCase):
     def test_validate_main_menu_1(self):
         self.user_interface = user_interface
         person = self.user_interface.validate_main_menu(1)
-        self.assertTrue(person)
+        self.assertEqual(person, (True, 1))
     
     def test_validate_main_menu_2(self):
         self.user_interface = user_interface
         person = self.user_interface.validate_main_menu(2)
-        self.assertTrue(person)
+        self.assertEqual(person, (True, 2))
     
     def test_validate_main_menu_3(self):
         self.user_interface = user_interface
         person = self.user_interface.validate_main_menu(3)
-        self.assertTrue(person)
+        self.assertEqual(person, (True,  3))
     
     def test_validate_main_menu_4(self):
         self.user_interface = user_interface
         person = self.user_interface.validate_main_menu(4)
-        self.assertTrue(person)    
+        self.assertEqual(person, (True, 4))    
     
     def test_not_valid_option(self):
         self.user_interface = user_interface
         person = self.user_interface.validate_main_menu(5)
-        self.assertTrue(person)
+        self.assertEqual(person, (False, None))
 
 class TestTryParseInt(unittest.TestCase):
     
@@ -88,32 +88,32 @@ class TestValidateCoinSelection(unittest.TestCase):
     def test_validate_coin_1(self):
         self.user_interface = user_interface
         coin = self.user_interface.validate_coin_selection(1)
-        self.assertEqual(coin, coin)
+        self.assertEqual(coin, (True, 'Quarter'))
     
     def test_validate_coin_2(self):
         self.user_interface = user_interface
         coin = self.user_interface.validate_coin_selection(2)
-        self.assertEqual(coin, coin)
+        self.assertEqual(coin, (True, 'Dime'))
     
     def test_validate_coin_3(self):
         self.user_interface = user_interface
         coin = self.user_interface.validate_coin_selection(3)
-        self.assertEqual(coin, coin)
+        self.assertEqual(coin, (True, 'Nickel'))
     
     def test_validate_coin_4(self):
         self.user_interface = user_interface
         coin = self.user_interface.validate_coin_selection(4)
-        self.assertEqual(coin, coin)
+        self.assertEqual(coin, (True, 'Penny'))
     
     def test_validate_coin_5(self):
         self.user_interface = user_interface
         coin = self.user_interface.validate_coin_selection(5)
-        self.assertEqual(coin, coin)
+        self.assertEqual(coin, (True, 'Done'))
     
     def test_not_valid_coin(self):
         self.user_interface = user_interface
         coin = self.user_interface.validate_coin_selection(6)
-        self.assertEqual(coin, coin)
+        self.assertEqual(coin, (False, None))
         
 if __name__ == '__main__':
     unittest.main()
